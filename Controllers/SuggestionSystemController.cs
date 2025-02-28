33fe588aed0753aded2ad43bf0dcb033be18176b
DWI IRENA DAMAYANTI <0320230010@polytechnic.astra.ac.id>
2025-02-28 13:18:15 +0700

    baru push awal

---
+++ Controllers/SuggestionSystemController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using sistem_informasi_lembaga_pengabdian_masyarakat_backend.Helper;
+using System.Data;
+
+namespace sistem_informasi_lembaga_pengabdian_masyarakat_backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class SuggestionSystemController(IConfiguration configuration) : Controller
+    {
+        readonly PolmanAstraLibrary.PolmanAstraLibrary lib = new(configuration.GetConnectionString("DefaultConnection"));
+        DataTable dt = new();
+
+
+        [HttpPost]
+        public IActionResult GetDataSuggestionSystem([FromBody] dynamic data)
+        {
+            try
+            {
+                JObject value = JObject.Parse(data.ToString());
+                dt = lib.CallProcedure("inn_getDataSuggestionSystem", EncodeData.HtmlEncodeObject(value));
+                return Ok(JsonConvert.SerializeObject(dt));
+            }
+            catch { return BadRequest(); }
+        }
+        //[Authorize]
+
+        //[Authorize]
+        [HttpPost]
+        public IActionResult CreateSuggestionSystem([FromBody] dynamic data)
+        {
+            try
+            {
+                JObject value = JObject.Parse(data.ToString());
+                dt = lib.CallProcedure("inn_createSuggestionSystem", EncodeData.HtmlEncodeObject(value));
+                return Ok(JsonConvert.SerializeObject(dt));
+            }
+            catch { return BadRequest(); }
+        }
+    }
+}
+
